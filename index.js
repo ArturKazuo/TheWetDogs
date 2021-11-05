d5613721d16a1d661c9cb570a937b1449abc6df1
ArturKazuo <afuzikawa@sga.pucminas.br>
2021-11-05 11:58:05 -0300

    #3 various additions and changes

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const target = document.querySelector('#inicio');
 const target2 = document.querySelector('#musicas');
+const target3 = document.querySelector('#sobre');
+const target4 = document.querySelector('#contato');
 
 const observer = new IntersectionObserver(entries => {
     entries.forEach(entry => {
@@ -34,10 +36,39 @@ const observer2 = new IntersectionObserver(entries => {
     })
 })
 
+const observer3 = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if(entry.isIntersecting){
+            document.querySelector('.titulo3').classList.remove('fadeOutRight');
+            document.querySelector('.sobreDiv').classList.remove('fadeOutLeft');
+            document.querySelector('.titulo3').classList.add('fadeInRight');
+            document.querySelector('.sobreDiv').classList.add('fadeInLeft');
+        }
+        else{
+            document.querySelector('.titulo3').classList.remove('fadeInRight');
+            document.querySelector('.sobreDiv').classList.remove('fadeInLeft');
+            document.querySelector('.titulo3').classList.add('fadeOutRight');
+            document.querySelector('.sobreDiv').classList.add('fadeOutLeft');
+        }
+    })
+})
 
-
-
-
+const observer4 = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if(entry.isIntersecting){
+            document.querySelector('.titulo4').classList.remove('fadeOutLeft');
+            document.querySelector('.contatoDiv').classList.remove('fadeOutRight');
+            document.querySelector('.titulo4').classList.add('fadeInLeft');
+            document.querySelector('.contatoDiv').classList.add('fadeInRight');
+        }
+        else{
+            document.querySelector('.titulo4').classList.remove('fadeInLeft');
+            document.querySelector('.contatoDiv').classList.remove('fadeInRight');
+            document.querySelector('.titulo4').classList.add('fadeOutLeft');
+            document.querySelector('.contatoDiv').classList.add('fadeOutRight');
+        }
+    })
+})
 
 
 
@@ -45,3 +76,7 @@ const observer2 = new IntersectionObserver(entries => {
 observer.observe(target);
 
 observer2.observe(target2);
+
+observer3.observe(target3);
+
+observer4.observe(target4);
